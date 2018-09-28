1d99a579d4b2ad8dd580b86246351531ff76b569
abstract-animation-9914 <vm930@hotmail.com>
2018-09-28 14:02:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 let companyName = "Scuber";
-let mostProfitableNeighborhood = "Chelsa";
+let mostProfitableNeighborhood = "Chelsea";
 let companyCeo =  "Susan Smith";
